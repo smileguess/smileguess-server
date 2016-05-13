2635581b48ed914b71533fc1353dc9b6cc9f18c1
travisCarson <travislcarson@gmail.com>
2016-05-13 12:49:02 -0700

    DOCS - Update docs for Users collection and User model (#16)

--- collections/Users.js
+++ collections/Users.js
@@ -1,29 +1,48 @@
 const User = require('../models/User');
 
-module.exports = class Users {
+/**
+* Creates a collection of Users
+*/
+class Users {
+  /**
+  * Defines properties for the Users collection
+  */
   constructor() {
     this.users = new Map();
     this.totalUsersCreated = 0;
   }
-
+  /**
+  * Instantiates new Users
+  * @params {string} - a User's unique device ID
+  */
   createUser(deviceId) {
     const newUser = new User(++this.totalUsersCreated, deviceId);
     this.addUserToCollection(newUser);
     return newUser;
   }
-
+  /**
+  * Inserts users into the collection
+  * @params {object} - a User instance
+  */
   addUserToCollection(newUser) {
     this.users.set(newUser.userId, newUser);
     return User;
   }
-
+  /**
+  * Removes a User from the collection
+  * @params {number} - a User ID
+  */
   destroy(userId) {
     this.users.delete(userId);
     return null;
   }
-
+  /**
+  * Retrieves a user from the collection
+  * @params {number} - a User ID
+  */
   getOne(userId) {
     return this.users.get(userId);
   }
-};
+}
 
+module.exports = Users;
