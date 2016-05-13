d5f603f8b6d6f80566b1492afbc5e517d0cb02b3
Travis Carson <travislcarson@gmail.com>
2016-05-13 12:45:37 -0700

    DOCS - Update game settings

--- gameSettings.js
+++ gameSettings.js
@@ -1,5 +1,19 @@
-module.exports = {
+const gamePlaySettings = {
+  /**
+  * Minimum viable numbers to start a game
+  * @type {number}
+  */
   minPlayers: 3,
+  /**
+  * Maximum number of players allowed in a game
+  * @type {number}
+  */
   maxPlayers: 6,
+  /**
+  * Regex defining which characters should not be considered in evaluating a guess
+  * @type {RegEx<object>}
+  */
   charsToAvoid: /-/g,
 };
+
+module.exports = gamePlaySettings;
