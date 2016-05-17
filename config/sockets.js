4c0c0d985a0eb27530b3d40337bd7e6da082cdcc
Travis Carson <travislcarson@gmail.com>
2016-05-16 19:51:35 -0700

    BUG - include messageController in sockets.js

--- config/sockets.js
+++ config/sockets.js
@@ -1,8 +1,8 @@
 const onConnect = require('../sockets/onConnect');
 const onDisconnect = require('../sockets/onDisconnect');
-const sendMessage = require('../sockets/sendMessage');
 const joinGame = require('../sockets/joinGame');
 const messageController = require('../controllers/messageController');
+
 
 module.exports = (io, db) => {
   io.on('connection', (socket) => {
@@ -13,7 +13,7 @@ module.exports = (io, db) => {
     socket.on('action', (action) => {
       switch (action.type) {
         case 'server/sendMessage':
-          return fieldMessage(db.games, action);
+          return messageController.fieldMessage(db.games, action);
         case 'server/joinGame':
           return joinGame(io, socket, action, db);
         default:
