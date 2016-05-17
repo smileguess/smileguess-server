1511cea9648600d8ec8f90d5a1d528970f59b8db
travisCarson <travislcarson@gmail.com>
2016-05-17 15:57:46 -0700

    BUG - If no deviceID is received from the client, one will be generated server-side (#36)

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,10 +1,12 @@
 const get = (userCollection, userId) => userCollection.getOne(userId);
+const randomInteger = require('./utils');
 
 const create = (req, res, userCollection, callback) => {
+  const deviceId = req.params.deviceId ? req.params.deviceId : randomInteger(128732, 2904895230);
   if (!callback) {
-    return userCollection.createUser(req.params.deviceId);
+    return userCollection.createUser(deviceId);
   }
-  return callback(userCollection.createUser(req.params.deviceId));
+  return callback(userCollection.createUser(deviceId));
 };
 
 const connect = (user, socket) => {
