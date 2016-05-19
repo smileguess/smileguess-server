4738080c9e2cd146463b8a158d773ca0c6df8da8
Will Wheeler <willfulbard@gmail.com>
2016-05-19 11:42:51 -0700

    FEAT - Remove toast for player join/leave events

--- spec/userControllerTests.js
+++ spec/userControllerTests.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const Users = require('../collections/Users');
 const UserController = require('../controllers/UserController');
-const ioCreate = require('socket.io');
-const app = express();
-const server = require('http').createServer(app);
-const io = ioCreate.listen(server);
-const onConnect = require('../sockets/onConnect');
-const onDisconnect = require('../sockets/onDisconnect');
+// const ioCreate = require('socket.io');
+// const app = express();
+// const server = require('http').createServer(app);
+// const io = ioCreate.listen(server);
+// const onConnect = require('../sockets/onConnect');
+// const onDisconnect = require('../sockets/onDisconnect');
 
 describe('User controller', () => {
   const users = new Users();
   const testUser = UserController.create({ params: 'anotherDeviceId' }, null, users);
-  let socket;
-  io.on('connection', (socket) => {
-    onConnect(io, socket);
-    socket = socket;
-    socket.on('disconnect', () => {
-      onDisconnect(io, socket);
-    });
-  });
+  const db = {
+    users,
+  };
+
+  // Mock socket connection object
+  let mockSocket = {
+    id: 'SomeFakeSocketID'
+  };
   it('should create users', () => {
     // this test user was created without making an API call
     // instantiation through an API call is tested in basicServerTests.js
@@ -26,7 +26,7 @@ describe('User controller', () => {
   });
   it('should impart users with a socket connection object', () => {
     expect(testUser.socket).toBe(null);
-    UserController.connect(testUser, socket);
+    UserController.connect(db, testUser, mockSocket);
     expect(testUser.socket).not.toBe(null);
   });
   it('should get users', () => {
